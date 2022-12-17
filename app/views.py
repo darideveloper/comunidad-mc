730773afa5076cfd39be6a87cae257373e67ab4d
darideveloper <darideveloper@gmail.com>
2022-12-17 01:24:33 -0600

    get user data after login with twitch

--- app/views.py
+++ app/views.py
@@ -1,9 +1,8 @@
-import json
-import requests
-import urllib.parse
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from dotenv import load_dotenv, dotenv_values
+from . import twitch
 
 # Get credentials
 config = dotenv_values(".env")
@@ -13,29 +12,29 @@ HOST = config["HOST"]
 
 # Create your views here.
 def login (request):
+    """ Manage login with twitch """
     
     current_path = f"{HOST}{request.path}"
     
-    # Get twitch token
-    token_url = "https://id.twitch.tv/oauth2/token"
-    res = requests.post (token_url, data={
-        "client_id": TWITCH_CLIENT_ID,
-        "client_secret": TWITCH_SECRET,
-        "grant_type": "client_credentials"
-    })
-    json_data = json.loads (res.content)
-    token = json_data["access_token"]
+    # Try to get login code from twitch, after login
+    login_code = request.GET.get("code", "")    
+    
+    if login_code: 
+        # Get twitch token for get user data
+        user_token, refresh_token = twitch.get_tokens(TWITCH_CLIENT_ID, TWITCH_SECRET, login_code, current_path)
+            
+        # Get user data
+        user_id, user_email, user_picture, user_name = twitch.get_user_info(user_token) 
+        print ()
+        print (user_id, user_email, user_picture, user_name)      
+        
+        
+        # Redirect to home page
+        # return redirect('index')
+        return HttpResponse(f"done")
 
     # Generate tiwtch login url
-    url_params = {
-        "response_type": token,
-        "client_id": TWITCH_CLIENT_ID,
-        "redirect_uri": current_path,
-        "response_type": "code",
-        "scope": "analytics:read:games bits:read channel:read:goals channel:read:polls channel:read:predictions channel:read:stream_key channel:read:subscriptions channel:read:vips moderation:read user:read:broadcast user:read:email user:read:follows user:read:subscriptions",
-    }
-    encoded_params = "&".join([f"{param_key}={param_value}" for param_key, param_value in url_params.items()])
-    twitch_link = f"https://id.twitch.tv/oauth2/authorize?{encoded_params}"
+    twitch_link = twitch.get_twitch_login_link(TWITCH_CLIENT_ID, current_path)
         
     return render (request, 'app/login.html', {
         "twitch_link":  twitch_link
