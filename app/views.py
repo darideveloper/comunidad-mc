129cfd742a69a6ca963a5dd9b1203eb0394017b5
darideveloper <darideveloper@gmail.com>
2022-12-16 23:14:12 -0600

    generate twitch login link

--- app/views.py
+++ app/views.py
@@ -1,29 +1,47 @@
+import json
+import requests
+import urllib.parse
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import load_dotenv, dotenv_values
 
 # Get credentials
-from dotenv import load_dotenv, dotenv_values
 config = dotenv_values(".env")
 TWITCH_CLIENT_ID = config["TWITCH_CLIENT_ID"]
 TWITCH_SECRET = config["TWITCH_SECRET"]
+HOST = config["HOST"]
 
 # Create your views here.
 def login (request):
     
-    # Generate twitch login url
-    twitch_link = f"https://id.twitch.tv/oauth2/authorize"
-    twitch_link += f"?response_type=code"
-    twitch_link += f"&client_id={TWITCH_CLIENT_ID}"
-    twitch_link += f"&redirect_uri=http://localhost:8000"
-    twitch_link += f"&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls"
-    twitch_link += f"&state=c3ab8aa609ea11e793ae92361f002671"
+    current_path = f"{HOST}{request.path}"
     
-    context = {
-        "twitch_link":  twitch_link
+    # Get twitch token
+    token_url = "https://id.twitch.tv/oauth2/token"
+    res = requests.post (token_url, data={
+        "client_id": TWITCH_CLIENT_ID,
+        "client_secret": TWITCH_SECRET,
+        "grant_type": "client_credentials"
+    })
+    json_data = json.loads (res.content)
+    token = json_data["access_token"]
+
+    # Generate tiwtch login url
+    url_params = {
+        "response_type": token,
+        "client_id": TWITCH_CLIENT_ID,
+        "redirect_uri": current_path,
+        "response_type": "code",
+        "scope": "analytics:read:games bits:read channel:read:goals channel:read:polls channel:read:predictions channel:read:stream_key channel:read:subscriptions channel:read:vips moderation:read user:read:broadcast user:read:email user:read:follows user:read:subscriptions",
     }
+    encoded_params = "&".join([f"{param_key}={param_value}" for param_key, param_value in url_params.items()])
+    twitch_link = f"https://id.twitch.tv/oauth2/authorize?{encoded_params}"
         
-    return render (request, 'app/login.html', context)
+    return render (request, 'app/login.html', {
+        "twitch_link":  twitch_link
+    })
     
 def index (request):
     return render (request, 'app/index.html')
+    # return HttpResponse (request)
     
