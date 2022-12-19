d2bae2abe9c9043e5c2d397cce42a31d6c768127
darideveloper <darideveloper@gmail.com>
2022-12-19 07:04:03 -0600

    rename project to comunidadmc

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn comunidad_mc.wsgi
+web: gunicorn comunidadmc.wsgi
--- comunidadmc/asgi.py
+++ comunidadmc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for comunidad_mc project.
+ASGI config for comunidadmc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comunidad_mc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comunidadmc.settings')
 
 application = get_asgi_application()
