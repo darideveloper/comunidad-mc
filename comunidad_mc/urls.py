dc8cfcb45fdea85ddcc0d2fd77fc6d31b96e8f07
darideveloper <darideveloper@gmail.com>
2022-12-19 06:57:40 -0600

    comunidad-mc.settings

--- comunidad_mc/urls.py
+++ comunidad_mc/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('', include('app.urls')),
