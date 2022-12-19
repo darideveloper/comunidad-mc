72ad7cab1e9bd1ee646ed082571b850ba3163676
darideveloper <darideveloper@gmail.com>
2022-12-18 18:53:17 -0600

    rename project

--- comunidad_mc/asgi.py
+++ comunidad_mc/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for twitch_time_x_time project.
+ASGI config for comunidad_mc project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitch_time_x_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comunidad_mc.settings')
 
 application = get_asgi_application()
--- comunidad_mc/wsgi.py
+++ comunidad_mc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for twitch_time_x_time project.
+WSGI config for comunidad_mc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitch_time_x_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comunidad_mc.settings')
 
 application = get_wsgi_application()
