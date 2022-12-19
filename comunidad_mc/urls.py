66317668fea49a02b22908296ba71725ed8340b1
darideveloper <darideveloper@gmail.com>
2022-12-19 06:09:39 -0600

    whitenoise for static files

--- comunidad_mc/urls.py
+++ comunidad_mc/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
