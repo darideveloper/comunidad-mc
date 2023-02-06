69841f6619f76fbec85ed89cb4654297ba8a71d1
darideveloper <darideveloper@gmail.com>
2023-02-05 22:22:15 -0600

    feat(sass) compile_sass.bat script

---
+++ app/static/app/css/compile_sass.bat
@@ -0,0 +1,10 @@
+echo Compaling files... & ^
+sass 404.sass 404.css & ^
+sass admin.sass admin.css & ^
+sass landing.sass landing.css & ^
+sass points.sass points.css & ^
+sass ranking.sass ranking.css & ^
+sass register.sass register.css & ^
+sass schedule.sass schedule.css & ^
+sass support.sass support.css & ^
+sass whatsapp.sass whatsapp.css
