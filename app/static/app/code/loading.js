3ecbc45847a1f55633ab3f4c52b765a7ef8fae95
darideveloper <darideveloper@gmail.com>
2023-02-05 22:24:41 -0600

    styles(loading) show loading spinner in page transitions

---
+++ app/static/app/code/loading.js
@@ -0,0 +1,27 @@
+// nodes
+const loading_wrapper = document.querySelector('.loading-wrapper')
+const menu_buttons = document.querySelectorAll('.menu-wrapper ul li a')
+
+console.log (menu_buttons)
+
+// Show the loading screen shwn click menu buttons
+menu_buttons.forEach (function (button) {
+  button.addEventListener ('click', e => {
+    // Evit redirect
+    e.preventDefault()
+
+    // Remove the hide class from the loading wrapper
+    loading_wrapper.classList.remove ('hide')
+
+    // Redirect to the page
+    const link = button.getAttribute ('href')
+    window.location.href = link
+  })
+})
+
+// Disable loading icon when the page is loaded
+window.addEventListener ('load', e => {
+  loading_wrapper.classList.add ('hide')
+})
+
+
