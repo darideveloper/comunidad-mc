abefb18b8f473ad41af5cb0338a0f8772e375358
darideveloper <darideveloper@gmail.com>
2023-02-08 08:43:08 -0600

    feat(models) vip stream

--- app/models.py
+++ app/models.py
@@ -84,6 +84,7 @@ class Stream (models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, name='user', verbose_name="usuario", help_text="usuario que está transmitiendo", null=False, blank=False)
     datetime = models.DateTimeField(name='datetime', verbose_name="fecha y hora", help_text="fecha y hora del stream", null=False, blank=False, default=timezone.now)
     is_free = models.BooleanField(name='is_free', verbose_name="free", help_text="indica si el stream es free (no restará puntos)", default=False)
+    is_vip = models.BooleanField(name='is_vip', verbose_name="vip", help_text="indica si el stream es vip (único en su hora)", default=False)
     
     def __str__(self):
         formated_date = str(timezone.localtime(self.datetime))[0:-12] + "h"
