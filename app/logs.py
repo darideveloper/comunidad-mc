bf90a91eb7415157acd2af6c5b8e2b864c0448fc
darideveloper <darideveloper@gmail.com>
2023-04-09 22:59:36 -0600

    chore (logs) change logs file name

--- app/logs.py
+++ app/logs.py
@@ -1,8 +1,7 @@
-import os
 import logging
 
 # logs to file
-logging.basicConfig(filename='app.log', format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s')
+logging.basicConfig(filename='.log', format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
