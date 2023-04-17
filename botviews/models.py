2d2914af4ec94f37da75a2a99589df2f2c1d8501
darideveloper <darideveloper@gmail.com>
2023-04-17 10:16:49 -0600

    feat (viwerbot/models) unique location name

--- botviews/models.py
+++ botviews/models.py
@@ -30,7 +30,7 @@ class Proxy (models.Model):
     
 class Location (models.Model):
     id = models.AutoField(primary_key=True, verbose_name='ID')
-    name = models.CharField(max_length=50, verbose_name='Nombre', help_text='Nombre de la ubicación')
+    name = models.CharField(max_length=50, verbose_name='Nombre', help_text='Nombre de la ubicación', unique=True)
     
     def __str__ (self):
         return self.name
