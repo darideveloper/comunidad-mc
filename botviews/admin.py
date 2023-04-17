965541159d8e2deccade76863f1573c926592cf4
darideveloper <darideveloper@gmail.com>
2023-04-16 21:29:17 -0600

    feat (botviews/admin) add proxy and location

--- botviews/admin.py
+++ botviews/admin.py
@@ -5,6 +5,24 @@ from django.contrib import admin
 class AdminUser (admin.ModelAdmin):
     
     list_display = ('id', 'name', 'cookies', 'is_active')
+    list_filter = ('is_active',)
     ordering = ('id', 'name', 'cookies', 'is_active')
     search_fields = ('cookies', 'is_active')
+    list_per_page = 20
+    
+@admin.register (models.Proxy)
+class AdminProxy (admin.ModelAdmin):
+    
+    list_display = ('id', 'host', 'port', 'user', 'password', 'location')
+    list_filter = ('location__name', )
+    ordering = ('id', 'host', 'port', 'user', 'password', 'location')
+    search_fields = ('host', 'port', 'user', 'password', 'location__name')
+    list_per_page = 20
+
+@admin.register (models.Location)
+class AdminLocations (admin.ModelAdmin):
+    
+    list_display = ('id', 'name')
+    ordering = ('id', 'name')
+    search_fields = ('name',)
     list_per_page = 20
--- botviews/models.py
+++ botviews/models.py
@@ -19,7 +19,7 @@ class Proxy (models.Model):
     port = models.IntegerField(verbose_name='Puerto', help_text='Puerto del proxy')
     user = models.CharField(max_length=50, verbose_name='Usuario', help_text='Usuario del proxy')
     password = models.CharField(max_length=50, verbose_name='Contraseña', help_text='Contraseña del proxy')
-    location = models.ForeignKey('Locations', on_delete=models.CASCADE, verbose_name='Ubicación', help_text='Ubicación del proxy', null=True, blank=True)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE, verbose_name='Ubicación', help_text='Ubicación del proxy', null=True, blank=True)
     
     def __str__(self):
         return f"{self.host}:{self.port}"
@@ -28,7 +28,7 @@ class Proxy (models.Model):
         verbose_name = "Proxy"
         verbose_name_plural = "Proxies"
     
-class Locations (models.Model):
+class Location (models.Model):
     id = models.AutoField(primary_key=True, verbose_name='ID')
     name = models.CharField(max_length=50, verbose_name='Nombre', help_text='Nombre de la ubicación')
     
