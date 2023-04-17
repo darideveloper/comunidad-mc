f7f4b9919302bd97e42835154a155bf2e9157260
darideveloper <darideveloper@gmail.com>
2023-04-16 21:15:03 -0600

    feat (botviews/models): proxy and location models

--- botviews/models.py
+++ botviews/models.py
@@ -5,10 +5,36 @@ class User (models.Model):
     name = models.CharField(max_length=50, verbose_name='Name', help_text='Nambre del usuario')
     cookies = models.JSONField(verbose_name='Cookies', help_text='Cookies de sesión del usuario')
     is_active = models.BooleanField(default=True, verbose_name='Activo', help_text='Indica si el usuario está activo')
-    
+
     def __str__(self):
         return self.name
     
     class Meta:
         verbose_name = "Usuario"
         verbose_name_plural = "Usuarios"
+
+class Proxy (models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='ID')
+    host = models.CharField(max_length=50, verbose_name='Host', help_text='Host del proxy')
+    port = models.IntegerField(verbose_name='Puerto', help_text='Puerto del proxy')
+    user = models.CharField(max_length=50, verbose_name='Usuario', help_text='Usuario del proxy')
+    password = models.CharField(max_length=50, verbose_name='Contraseña', help_text='Contraseña del proxy')
+    location = models.ForeignKey('Locations', on_delete=models.CASCADE, verbose_name='Ubicación', help_text='Ubicación del proxy', null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.host}:{self.port}"
+    
+    class Meta:
+        verbose_name = "Proxy"
+        verbose_name_plural = "Proxies"
+    
+class Locations (models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='ID')
+    name = models.CharField(max_length=50, verbose_name='Nombre', help_text='Nombre de la ubicación')
+    
+    def __str__ (self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Ubicación"
+        verbose_name_plural = "Ubicaciones"
