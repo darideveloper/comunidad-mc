3043a9b224d009876aeb6e83a82c1a4a604dba48
darideveloper <darideveloper@gmail.com>
2023-04-20 05:05:13 -0600

    feat (botviews/views): validate token

---
+++ botviews/decorators.py
@@ -0,0 +1,20 @@
+from . import models
+from functools import wraps
+from django.http import HttpResponseBadRequest
+
+
+def validate_token (function):
+    """ View wrapper for return data only if the token it's valid """
+    
+    @wraps(function)
+    def wrap (request, *args, **kwargs):
+        
+        # Validate token
+        token = request.GET.get ("token")
+        token_found = models.Token.objects.filter (value=token)
+        if token_found and token_found.first().is_active:
+            return function(request, *args, **kwargs)
+        else:
+            return HttpResponseBadRequest ("Invalid token")
+        
+    return wrap
--- botviews/views.py
+++ botviews/views.py
@@ -2,6 +2,7 @@ import os
 import django
 from dotenv import load_dotenv
 from . import models
+from . import decorators
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
@@ -30,30 +31,35 @@ def get_json_model (model:django.db.models) -> str:
     
     return objects_json
 
+@decorators.validate_token
 def get_settings (request):
     """ Returns all settings in json format """        
     return HttpResponse(get_json_model(models.Setting), content_type='application/json')
 
+@decorators.validate_token
 def get_proxies (request):
     """ Returns all proxies in json format """    
 
     return HttpResponse(get_json_model(models.Proxy), content_type='application/json')
 
+@decorators.validate_token
 def get_users (request):
     
     """ Returns all users in json format """
         
     return HttpResponse(get_json_model(models.User), content_type='application/json')
 
+@decorators.validate_token
 def get_locations (request):
     
     """ Returns all locations in json format """
     
     return HttpResponse(get_json_model(models.Location), content_type='application/json')
-    
+
+@decorators.validate_token
 def get_streams (request):
     """ Returns names of the current streamers in comunidad mc, as json format """
-    
+        
     # get current streams
     twitch = TwitchApi()
     streams = twitch.get_current_streams ()
