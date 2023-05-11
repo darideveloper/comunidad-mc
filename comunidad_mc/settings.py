64a7189e474171ebdd1c1e66852736e09bdaf856
darideveloper <darideveloper@gmail.com>
2023-05-11 10:54:14 -0600

    fix (settings): SECRET_KEY in .env

--- comunidad_mc/settings.py
+++ comunidad_mc/settings.py
@@ -19,9 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "jhvasuyfedabjhwerjhvcdsiuygqw768t3214iug341287t12e3bjdqsasc"
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
 
 # Application definition
