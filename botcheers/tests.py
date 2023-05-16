5e834c31a0f4f7720d2dc5ad3d2e44fecf2d0a43
darideveloper <darideveloper@gmail.com>
2023-05-15 21:06:45 -0600

    feat (botcheers/test): test admin home and admin donations

--- botcheers/tests.py
+++ botcheers/tests.py
@@ -1,8 +1,8 @@
 from . import models
 from django.test import TestCase
-from django.contrib.auth.models import User as UserAuth, Group
 from django.utils import timezone
-from django.urls import reverse
+from django.contrib.auth.models import User as UserAuth, Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 class TestModels (TestCase):
     
@@ -26,7 +26,7 @@ class TestModels (TestCase):
         # Create test instances
         self.user_auth = UserAuth.objects.create_user (
             email="sample@gmail.com",
-            username="sample_auth",
+            username="sample auth",
             password="sample",
             is_staff=True,
             is_superuser=True
@@ -75,7 +75,7 @@ class TestModels (TestCase):
         self.assertEqual (donation.minute, self.donation_minute)
         self.assertEqual (donation.amount, self.donation_amount)
         self.assertEqual (donation.message, self.donation_message)
-        self.assertEqual (donation.__str__(), "test_user - 10 bits (sample_auth)")
+        self.assertEqual (donation.__str__(), "test_user - 10 bits (sample auth)")
         
     
     def test_token (self):
@@ -110,7 +110,7 @@ class TestViews (TestCase):
         # Create test instances
         self.user_auth = UserAuth.objects.create_user (
             email="sample@gmail.com",
-            username="sample_auth",
+            username="sample auth",
             password="sample",
             is_staff=True,
             is_superuser=True
@@ -186,33 +186,217 @@ class TestViews (TestCase):
         self.assertEqual (response.status_code, 200)
         self.assertEqual (response.json(), [])
     
-# class TestAdmin (TestCase):
+class TestAdmin (TestCase):
     
-#     def setUp (self):
+    def setUp (self):
     
-#         # Create admin instances
-#         self.user_auth = UserAuth.objects.create_user (
-#             email="sample@gmail.com",
-#             username="sample_auth",
-#             password="sample",
-#             is_staff=True,
-#             is_superuser=True
-#         )
+        # Test endpoints
+        self.endpoint_admin = "/admin"
+        self.endpoint_admin_login = f"{self.endpoint_admin}/login/"
+        self.endpoint_app = f"{self.endpoint_admin}/botcheers/"
+        self.endpoint_admin_donation_list = f"{self.endpoint_app}donation/"
+        self.endpoint_admin_donation_add = f"{self.endpoint_app}donation/add/"
     
-#         self.group_bot_cheers_manager = Group.objects.create (
-#             name = "Bot Cheers Manager"
-#         )
+        # Create admin instances
+                
+        # Create admin users
+        self.user_auth_email = "sample@gmail.com"
+        self.user_auth_username = "sample auth"
+        self.user_auth_username_pro = "sample auth pro"
+        self.user_auth_password = "sample pass"
         
-#         # Create cheerbot instances
-#         self.user = models.User.objects.create (
-#             name = "test_user",
-#             cookies = [{"test": "test"}],
-#             is_active = True,
-#             user_auth = self.user_auth,
-#         )
+        self.user_auth = UserAuth.objects.create_user (
+            email=self.user_auth_email,
+            username=self.user_auth_username,
+            password=self.user_auth_password,
+            is_staff=True,
+        )
         
-#         self.token = models.Token.objects.create (
-#             name = "test_token",
-#             value = "test_token_value",
-#             is_active = True,
-#         )
+        self.user_auth_pro = UserAuth.objects.create_user (
+            email=self.user_auth_email,
+            username=self.user_auth_username_pro,
+            password=self.user_auth_password,
+            is_staff=True,
+        )
+    
+        # Create admin groups
+        self.group_bot_cheers_manager = Group.objects.create (
+            name = "bot cheers manager"
+        )
+        
+        self.group_bot_cheers_manager_pro = Group.objects.create (
+            name = "bot cheers manager pro"
+        )
+        
+        # Set permissions to groups
+        content_type_user = ContentType.objects.get_for_model (models.User)
+        content_type_donations = ContentType.objects.get_for_model (models.Donation)
+        
+        permissions = []
+        permissions += Permission.objects.filter (content_type=content_type_user)        
+        permissions += Permission.objects.filter (content_type=content_type_donations)
+        
+        for permision in permissions:
+            self.group_bot_cheers_manager.permissions.add (permision)
+            self.group_bot_cheers_manager_pro.permissions.add (permision)
+        
+        # Add groups to users
+        self.user_auth.groups.add (self.group_bot_cheers_manager)
+        self.user_auth_pro.groups.add (self.group_bot_cheers_manager_pro)       
+        
+        # Create cheerbot instances
+        
+        # Create cheerbot users       
+        self.user = models.User.objects.create (
+            name = "test user",
+            cookies = [{"test": "test"}],
+            is_active = True,
+            user_auth = self.user_auth,
+        )
+        
+        self.user_pro = models.User.objects.create (
+            name = "test user pro",
+            cookies = [{"test": "test"}],
+            is_active = True,
+            user_auth = self.user_auth_pro,
+        )
+        
+        # Create cheerbot donations
+        self.donation_stream_chat_link = "https://www.twitch.tv/popout/auronplay/chat?popout="
+        self.donation_hour = timezone.localtime(timezone.now()).hour
+        self.donation_minute = 1
+        self.donation_amount = 10
+        
+        self.donation_regular = models.Donation.objects.create (
+            user = self.user,
+            stream_chat_link = self.donation_stream_chat_link,
+            hour = self.donation_hour,
+            minute = self.donation_minute,
+            amount = self.donation_amount,
+            message = "donation regular user",
+        )
+        
+        self.donation_pro = models.Donation.objects.create (
+            user = self.user_pro,
+            stream_chat_link = self.donation_stream_chat_link,
+            hour = self.donation_hour + 1 ,
+            minute = self.donation_minute,
+            amount = self.donation_amount,
+            message = "donation pro user",
+        )
+        
+        # Create cheerbot tokens
+        self.token = models.Token.objects.create (
+            name = "test_token",
+            value = "test_token_value",
+            is_active = True,
+        )
+        
+    def login (self, user:str, password:str):
+        """ Login to admin with user and password
+
+        Args:
+            user (str): username to login
+            password (str): password of the user
+        """
+        
+        # Login in admin with user and password
+        response = self.client.post (self.endpoint_admin_login, {
+            "username": user, 
+            "password": password
+        }, follow=True)
+        
+        # Test redirect after login
+        self.assertEqual(response.status_code, 200)
+        
+    def test_admin_regular (self):
+        """ Test admin home page to regular user
+        """
+        
+        # Login
+        self.login (self.user_auth_username, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin, follow=True)
+        self.assertEqual(response.status_code, 200)
+        
+        # Validate available tables of response: it should show botcheers tables
+        self.assertContains(response, "Botcheers</a>")
+        self.assertContains(response, "Bots</a>")
+        self.assertContains(response, "Donaciones</a>")
+        
+    def test_admin_pro (self):
+        """ Test admin home page to pro user
+        """
+        
+        # Login
+        self.login (self.user_auth_username_pro, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin, follow=True)
+        self.assertEqual(response.status_code, 200)
+        
+        # Validate available tables of response: it should show botcheers tables
+        self.assertContains(response, "Botcheers</a>")
+        self.assertContains(response, "Bots</a>")
+        self.assertContains(response, "Donaciones</a>")
+
+    def test_donation_list_regular (self):
+        """ Tets donation list page to regular user
+        """
+        
+        # Login
+        self.login (self.user_auth_username, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin_donation_list)
+        self.assertEqual(response.status_code, 200)
+                
+        # Validate rows of response: it should show only regular user donations
+        self.assertContains(response, self.donation_regular.message)
+        self.assertNotContains(response, self.donation_pro.message)
+        
+    def test_donation_list_pro (self):
+        """ Tets donation list page to pro user
+        """
+        
+        # Login
+        self.login (self.user_auth_username_pro, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin_donation_list)
+        self.assertEqual(response.status_code, 200)
+        
+        # Validate rows of response: it should show all user donations
+        self.assertContains(response, self.donation_regular.message)
+        self.assertContains(response, self.donation_pro.message)
+    
+    def test_donation_add_regular (self):
+        """ Tets add donation page to regular user
+        """
+        
+        # Login
+        self.login (self.user_auth_username, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin_donation_add)
+        self.assertEqual(response.status_code, 200)
+        
+        # Validate rows of response: user options should be only regular user
+        self.assertContains(response, self.user.name)
+        self.assertNotContains(response, self.user_pro.name)
+        
+    def test_donation_add_pro (self):
+        """ Tets adc donation page to pro user
+        """
+        
+        # Login
+        self.login (self.user_auth_username_pro, self.user_auth_password)
+        
+        # Validate response
+        response = self.client.get (self.endpoint_admin_donation_add)
+        self.assertEqual(response.status_code, 200)
+        
+        # Validate rows of response: user options should be all users
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user_pro.name)
