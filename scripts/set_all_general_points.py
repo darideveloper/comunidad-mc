e672f6861cf2d50474214bf4e35fd8ecf0941a9c
daridev <darideveloper@gmail.com>
2023-07-01 00:17:18 -0600

    feat (scripts/set_all_general_points): select stream by id and delete old general points

--- scripts/set_all_general_points.py
+++ scripts/set_all_general_points.py
@@ -1,13 +1,12 @@
 # Set specific general and today points to all users, in specific stream
 
 # UPDATE THIS:
-STREAMER_NAME = "darideveloper2"
-POINTS = 5
+STREAMS_IDS = [2919, 2916]
+POINTS = 1
 
 # Add parent folder to path
 import os
 import sys
-import random
 from django.utils import timezone
 parent_folder = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(parent_folder)
@@ -25,24 +24,26 @@ users = models.User.objects.all()
 twitch_api = TwitchApi ()
 
 # Get streamers
-streamer = models.User.objects.filter(user_name=STREAMER_NAME).first ()
+streams = models.Stream.objects.filter(id__in=STREAMS_IDS)
 
-# Get last streams of streamer
-stream = models.Stream.objects.filter(user=streamer).order_by('-id').first()
-print (f"\nStream: {stream}\n")
+for stream in streams:
 
-# Loop users for add points
-for user_name in users: 
-    
-    # Get user
-    user = models.User.objects.filter(user_name=user_name).first()
-    
-    # Skip if user not found
-    if not user:
-        continue
-    
-    # add 10 points today
-    for _ in range (POINTS):
-        twitch_api.add_cero_point (user, stream, force=True)
-        twitch_api.add_point(user, stream, force=True)
+    print (f"\nStream: {stream}\n")
+
+    # Loop users for add points
+    for user_name in users: 
+        
+        # Get user
+        user = models.User.objects.filter(user_name=user_name).first()
+        
+        # Skip if user not found
+        if not user:
+            continue
+        
+        # Delete if user already have a point in the stream
+        models.GeneralPoint.objects.filter(user=user, stream=stream).delete ()
+        
+        # add 10 points today
+        for _ in range (POINTS):
+            twitch_api.add_point(user, stream, force=True)
     
