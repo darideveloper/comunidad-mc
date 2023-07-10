9ce0be7c7dbbd669bbdef38babc6ae48f90065e0
daridev <darideveloper@gmail.com>
2023-07-10 14:13:09 -0600

    feat (scripts/set_all_general_points_force): use stream ids and delete old points

--- scripts/set_all_general_points_force.py
+++ scripts/set_all_general_points_force.py
@@ -1,14 +1,12 @@
 # Set specific general and today points to all users, in specific stream
 
 # UPDATE THIS:
-STREAMER_NAME = "darideveloper2"
-POINTS = 5
+SREAMS_IDS = [3299, 3323]
+POINTS = 1
 
 # Add parent folder to path
 import os
 import sys
-import random
-from django.utils import timezone
 parent_folder = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(parent_folder)
 
@@ -25,25 +23,34 @@ users = models.User.objects.all()
 twitch_api = TwitchApi ("Script")
 
 # Get streamers
-streamer = models.User.objects.filter(user_name=STREAMER_NAME).first ()
+streams = models.Stream.objects.filter (id__in=SREAMS_IDS)
 
 # Get last streams of streamer
-stream = models.Stream.objects.filter(user=streamer).order_by('-id').first()
-print (f"\nStream: {stream}\n")
+for stream in streams:
+    print (f"\nStream: {stream}\n")
 
-# Loop users for add points
-for user in users: 
-    
-    print (f"Adding points to {user.user_name} in {stream}")
-
-    general_point = models.GeneralPoint (
-        user=user,
-        stream=stream,
-        amount=POINTS,
-        info=models.InfoPoint.objects.get (info="ver stream"),
-    )
-    general_point.save ()
-    
-    models.DailyPoint.objects.create (
-        general_point=general_point
-    )
+    # Loop users for add points
+    for user in users: 
+        
+        # Delete old general point
+        general_point = models.GeneralPoint.objects.filter (
+            stream=stream,
+            user=user,
+        ).delete ()
+        
+        models.Log.objects.create (
+            origin=models.LogOrigin.objects.get (name="Script"),
+            details=f"Adding {POINTS} points to {user.user_name} in {stream}",
+        )
+        
+        general_point = models.GeneralPoint (
+            user=user,
+            stream=stream,
+            amount=POINTS,
+            info=models.InfoPoint.objects.get (info="ver stream"),
+        )
+        general_point.save ()
+        
+        models.DailyPoint.objects.create (
+            general_point=general_point
+        )
