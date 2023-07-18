e1c5384fbb46d36c676c55699818091000d9ea6c
daridev <darideveloper@gmail.com>
2023-07-17 18:08:53 -0600

    feat (crons): backup_db.bat

---
+++ crons/backup_db.bat
@@ -0,0 +1,3 @@
+set PGPASSWORD=2nMIe8McUCcbDhPPGUCNpJC2iNgKDtP5
+
+C:\"Program Files"\PostgreSQL\15\bin\pg_dump.exe -d "comunidad_mc" -h "dpg-cef3eqsgqg4b3haa6eq0-a.ohio-postgres.render.com" -U "daridev" -f E:\full_stack\comunidad_mc\sql\backups\backup
