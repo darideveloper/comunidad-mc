a1a58ef43cc9378e08af23eb65c516b2eb433b99
daridev <darideveloper@gmail.com>
2023-12-04 19:02:55 -0600

    feat (settings): set python version

--- comunidad_mc/settings.py
+++ comunidad_mc/settings.py
@@ -47,10 +47,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
